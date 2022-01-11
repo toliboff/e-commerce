1211ec559061df175a099a9cc6886538af1b72cf
Tolibjon <tolibjohn@gmail.com>
2022-01-11 21:39:38 +0500

    Create styled components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+`
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+      navbar
+    </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
