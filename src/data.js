c3724e15e32ed38108b0deede8dafe6848afc8ed
Tolibjon <tolibjohn@gmail.com>
2022-01-12 11:55:13 +0500

    Create data file for slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    title: 'AUTUMN SALE',
+    description: 'DONT\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg:'fff',
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1502163140606-888448ae8cfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    title: 'SUMMER SALE',
+    description: 'DONT\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg:'fff',
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+    title: 'WINTER SALE',
+    description: 'DONT\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg:'fff',
+  },
+];
