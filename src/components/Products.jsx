e5ca15058527224455cfc70fee633ec40474731c
Tolibjon <tolibjohn@gmail.com>
2022-01-13 11:25:19 +0500

    Create Products component

---
+++ src/components/Products.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { popularProducts } from '../data';
+import Product from './Product';
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`
+
+const Products = () => {
+  return (
+    <Container>
+      {
+        popularProducts.map((item) => <Product key={item.id} item={item} />)
+      }
+    </Container>
+  )
+}
+
+export default Products
