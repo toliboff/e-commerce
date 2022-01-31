115f3df2aea1246d3f223e780dfd7a6af979fc18
Tolibjon <tolibjohn@gmail.com>
2022-01-31 17:24:13 +0500

    Create Payment page and integrate Stripe payment

---
+++ src/pages/Pay.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+ const KEY = "pk_test_51KNy4ILGkM0FHNqs9CsmvTpDBxdnvLts5331kBh6Ujszk4AKnWsdpUKgQLB4OmtITUYKFWTEl26YETioAZmIRu7C00sAphs6EB";
+
+
+const Pay = () => {
+ const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = token => {
+    setStripeToken(token);
+  }
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try{
+        axios
+      } catch (err){
+        console.log(err)
+      }
+    }
+  }, [stripeToken]);
+
+  return <div>
+    <h1>Pay</h1>
+    <StripeCheckout
+      name="E-Commerce"
+      image="https://drive.google.com/file/d/1N3DGtx3Fy7vei3C_PDF5FS5NlTMld_I-/view?usp=sharing"
+      billingAddress
+      shippingAddress
+      description="Your total is $30"
+      amount={3000}
+      token={onToken}
+      stripeKey={KEY} 
+    >
+      <button
+        style={{
+          padding: '5px 10px',
+          backgroundColor: "blue",
+          color: "white",
+          cursor: "pointer",
+          border: "none",
+          borderRadius: "5px",
+        }}
+      >Pay Now</button>
+    </StripeCheckout>
+  </div>;
+};
+
+export default Pay;
