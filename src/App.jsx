2aa9fdfdd71a0b6ffeae6a567666a311ae31b05f
Tolibjon <tolibjohn@gmail.com>
2022-01-31 17:24:44 +0500

    Add routes, create Success page for payment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Pay from "./pages/Pay";
+import Success from "./pages/Success";
 
 function App() {
-  return <Cart />
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Pay />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
+  </Router>
 }
 
 export default App;
