f468f7b74df9b19d448d3624439a42a34978b8e1
Tolibjon <tolibjohn@gmail.com>
2022-02-09 20:29:26 +0500

    Fix image link

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -70,7 +70,7 @@ const Product = ({item}) => {
   return (
     <Container>
       <Circle />
-      <Image src={item.img} />
+      <Image src={item.image} />
       <Info>
         <Icon>
           <ShoppingCartOutlined />
