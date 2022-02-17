c2de6767d808458a2af7bae0ca45816300e4e7fc
Tolibjon <tolibjohn@gmail.com>
2022-02-17 14:02:37 +0500

    Create user reducer and actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 import cartReducer from './cartRedux';
+import userReducer from './userRedux';
 
 export default configureStore({
-  reducer: {cartReducer}
+  reducer: {cartReducer, userReducer}
 });
